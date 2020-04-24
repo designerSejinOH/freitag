406012f19f212b17ba147e4347c275417577a4ba
SejinOH <39869184+SejinO@users.noreply.github.com>
2020-04-25 06:19:38 +0900

    s

--- sketch.js
+++ sketch.js
@@ -2,41 +2,45 @@ let truck;
 let ma;
 let maxD;
 let x = 0;
-let y =0;
+let y = 0;
+
 function preload() {
   truck = loadModel('trruck_c4d.obj');
-  
+
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
-  x = width/2-100;
+  x = width / 2 - 150;
   ma = atan(1 / sqrt(2));
 
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
 function draw() {
-  background(200);
-  ortho(-300, 300, 300,- 300, 0, 1000);
+  background(255);
+  ortho(-300, 300, 300, -300, 0, 1000);
   rotateX(ma);
   rotateY(-QUARTER_PI);
   push();
   scale(2);
-  translate(x, y, 0);
-  normalMaterial(); // For effect
+  translate(x, y, -20);
+  fill(0, 255, 0);
   model(truck);
   pop();
-   push();
-   scale(2);
-   translate(x, y, 50);
-   normalMaterial(); // For effect
-   model(truck);
-   pop();
+  push();
+  scale(2);
+  translate(-x, y, 20);
+  rotateY(PI);
+  fill(255, 0, 0);
+  model(truck);
+  pop();
   x--;
-  if (x < - width / 2+100) {
-    x = width / 2-100;
+  if (x < -width / 2 + 150) {
+    x = width / 2 - 150;
   }
   print('The value of x is ' + x);
 
